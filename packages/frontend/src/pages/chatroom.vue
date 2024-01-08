0b24b63bfda2e807b7056d307bd8ec5e231dc5a7
rafucuk <raufsen11@gmail.com>
2024-01-08 08:56:36 +0000

    tried to add streaming, TODO STREAMING.

--- packages/frontend/src/pages/chatroom.vue
+++ packages/frontend/src/pages/chatroom.vue
@@ -1,7 +1,7 @@
 <template>
     <MkStickyContainer>
         <MkSpacer :contentMax="800">
-            <MkNotes :pagination="directNotesPagination"/>
+            <MkNotes ref="tlComponent" :noGap="!defaultStore.state.showGapBetweenNotesInTimeline" :pagination="directNotesPagination" @queue="emit('queue', $event)"/>
         </MkSpacer>
     </MkStickyContainer>
     </template>
@@ -11,15 +11,46 @@
     import MkNotes from '@/components/MkNotes.vue';
     import { i18n } from '@/i18n';
     import { definePageMetadata } from '@/scripts/page-metadata';
-    
+    import { useStream } from '@/stream';
+    import * as sound from '@/scripts/sound';
+    import { $i } from '@/account';
+    import { defaultStore } from '@/store';
+
+    let connection;
+
+    const stream = useStream();
+
+
+    const emit = defineEmits<{
+        (ev: 'note'): void;
+        (ev: 'queue', count: number): void;
+    }>();
+
     const props = defineProps<{
         noteId: string;
+        sound?: boolean;
     }>();
+
+    const tlComponent: InstanceType<typeof MkNotes> = $ref();
     
+    const prepend = note => {
+        console.log('testt')
+        tlComponent.pagingComponent?.prepend(note);
+
+        emit('note');
+
+        if (props.sound) {
+            sound.play($i && (note.userId === $i.id) ? 'noteMy' : 'note');
+        }
+    };
+
+	connection = stream.useChannel('globalTimeline');
+	connection.on('note', prepend);
+        
     const directNotesPagination = {
         reversed: true,
         endpoint: 'notes/thread' as const,
-        limit: 5,
+        limit: 10,
         params: {
             threadId: props.noteId,
         },
